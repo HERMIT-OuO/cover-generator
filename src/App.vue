4b23e483ea316f16e71d07bc5cecbfa4265cda40
HERMIT <ruoliangty@gmail.com>
2022-05-09 17:55:56 +0800

    feat: Init Project.

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="app">
+      <h1>Make your cover here.</h1>
+      <Card />
+    </div>
+</template>
+
+<script lang='ts'>
+import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+import  Card  from './components/Card.vue';
+interface DataProps {}
+export default {
+    name: "",
+    components: {
+      Card
+    },
+    setup() {
+        const data: DataProps = reactive({});
+        onBeforeMount(() => {});
+        onMounted(() => {});
+        const refData = toRefs(data);
+        return {
+            ...refData,
+        };
+    },
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <div class="">
+        Card
+    </div>
+</template>
+
+<script lang='ts'>
+import { reactive, toRefs, onBeforeMount, onMounted } from "vue";
+interface DataProps {}
+export default {
+    name: "Card",
+    setup() {
+        const data: DataProps = reactive({});
+        onBeforeMount(() => {});
+        onMounted(() => {});
+        const refData = toRefs(data);
+        return {
+            ...refData,
+        };
+    },
+};
+</script>
+<style scoped>
+</style>
